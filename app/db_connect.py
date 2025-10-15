5cd77a37a4aae2679c6a4277931a74fbce1fc165
Kent1 <kent12710@gmail.com>
2025-10-15 22:03:55 +0700

    Thêm file connect.py

---
+++ app/db_connect.py
@@ -0,0 +1,17 @@
+import pyodbc
+from tkinter import messagebox
+
+def get_db_connect():
+    try:
+        # ====Kết nối tới sql server====
+        conn = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server};'
+            'SERVER=LAPTOP_ASUS\SQLEXPRESS;'
+            'DATABASE=QLDiemSV;'
+            'UID=sa;'
+            'PWD=;'
+        )
+        return conn
+    except Exception as e:
+        messagebox.showerror("Lỗi kết nối", str(e))
+        return None
